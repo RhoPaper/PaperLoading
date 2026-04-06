80f07be0dad1cbf3725605e1f1873fd07931d6a5
RhoPaper <rzj44346@outlook.com>
2026-04-07 06:57:52 +0800

    delete Kimi's code and restart with myself now center a black div

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@ import PixelLoading from './components/pixel_loading.vue'
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
     <PixelLoading />
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 </template>
 
 <style scoped></style>
