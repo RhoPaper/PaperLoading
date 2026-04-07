620be519ac70e19cf5dae43a418a25b1f90a04a7
RhoPaper <rzj44346@outlook.com>
2026-04-07 17:35:43 +0800

    重构pixel_loading.vue 组件，添加加载动画逻辑并更新样式；新增 main.css 文件以解耦定义加载宽度变量

--- src/components/pixel_loading.vue
+++ src/components/pixel_loading.vue
@@ -1,19 +1,46 @@
 <script setup>
+import { onMounted } from 'vue';
 
+const afterLoadingWidth = '100px';
+var isLoading = false;
+
+const LoadingAnimation = (element, targetWidth) => {
+    if (element && isLoading) {
+        element.style.width = targetWidth;
+    }
+};
+
+const onLoading = () => {
+    let loadingDOM = window.document.getElementById("pixel-loading");
+    setTimeout(() => {
+        isLoading = true;
+        LoadingAnimation(loadingDOM, afterLoadingWidth);
+    }, 100);
+    setTimeout(() => {
+        console.log('Loading completed');
+        isLoading = false;
+    }, 1000);
+};
+
+onMounted(() => {
+    onLoading();
+});
 </script>
 
 <template>
-    <div class="pixel-loading"> </div>
+    <div id="pixel-loading"></div>
 </template>
 
 <style scoped>
-.pixel-loading {
+#pixel-loading {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    /* translateX(0) 让左边缘对齐中心点，宽度向右增长 */
+    transform: translate(0, -50%);
     width: 10px;
     height: 10px;
     background-color: #000;
+    transition: width .5s ease;
 }
 </style>
